1220bebd8d6d76e0a7c4779b8084550fa4e0a5c6
凉晨lky <16185443+liang-chen-lky@user.noreply.gitee.com>
2026-07-10 23:47:59 +0800

    feat: wire restored player movement animations

--- Assets/Scrips/Characters/Player/Data/States/Grounded/Moving/PlayerWalkData.cs
+++ Assets/Scrips/Characters/Player/Data/States/Grounded/Moving/PlayerWalkData.cs
@@ -7,8 +7,4 @@ public class PlayerWalkData
     [field: SerializeField]
     [field: Range(0f, 1f)]
     public float SpeedModifier { get; private set; } = 0.225f;
-
-    [field: SerializeField]
-    [field: Range(0.1f, 1f)]
-    public float AnimationSpeedModifier { get; private set; } = 0.55f;
 }

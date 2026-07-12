af6e7cbceb3e5f435c166d0520d9c27eb363e612
凉晨lky <16185443+liang-chen-lky@user.noreply.gitee.com>
2026-07-12 15:08:39 +0800

    fix: ignore attack input while airborne

--- Assets/Scrips/Characters/Player/Statemachine/Movement/State/AirBorne/PlayerAirborneState.cs
+++ Assets/Scrips/Characters/Player/Statemachine/Movement/State/AirBorne/PlayerAirborneState.cs
@@ -1,3 +1,5 @@
+using UnityEngine.InputSystem;
+
 public class PlayerAirborneState : PlayerMovementState
 {
     public PlayerAirborneState(PlayerMovementStateMachine stateMachine) : base(stateMachine)
@@ -14,6 +16,10 @@ public class PlayerAirborneState : PlayerMovementState
     #endregion
 
     #region Reusable Methods
+
+    protected override void OnAttackStarted(InputAction.CallbackContext context)
+    {
+    }
 
     protected override void OnContactWithGround()
     {
